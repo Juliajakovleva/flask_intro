356d16d6ff9fe7c49bcc0461abdb11bdfa130e21
Juliajakovleva <59760338+Juliajakovleva@users.noreply.github.com>
2021-09-23 23:21:25 +0300

    flask basic

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Sveika, sestdiena!')
+from flask import Flask, app
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Sveika, svetdiena!"
+
+@app.route('/kontakti')
+def kontakti():
+    return "<html><h1>Kontakti</h1><p>Yulia Yakovleva</p></html>"
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
